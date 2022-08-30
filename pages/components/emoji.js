48e94c792888ab34bc60e5bbc126eebbd57a6ff2
Gerónimo Pericoli <geronimopericoli@gmail.com>
2022-08-29 21:06:56 -0300

    first commit

--- pages/components/emoji.js
+++ pages/components/emoji.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const Emoji = props => (
     <span
         className="emoji"
@@ -9,4 +10,5 @@ const Emoji = props => (
         {props.symbol}
     </span>
 );
+
 export default Emoji;
